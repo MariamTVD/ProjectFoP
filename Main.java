1fba767a402f42db45e957e0c261c57c1c021774
TeklaTsitelishvili <tsitelishvilitekla123@gmail.com>
2024-12-28 02:20:21 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
+
+    public static Map<String, Integer> values = new HashMap<>();
+
+      // Removing unnecessary empty lines from user input
+    public static String[] removeEmptyLines(String[] lines) {
+        List<String> nonEmptyLines = new ArrayList<>();
+        for (String line : lines) {
+            // Check if the line is not empty or there are only whitespaces
+            if (!line.trim().isEmpty()) {
+                nonEmptyLines.add(line);
+            }
+        }
+        return nonEmptyLines.toArray(new String[0]);
+    }
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         StringBuilder input = new StringBuilder();
@@ -12,7 +27,23 @@ public class Main {
             if(line.equals("end")) break;
             input.append(line);
             input.append("\n");
-
         }
+   String[] linesOfInput = input.toString().split("\n");
+        linesOfInput = removeEmptyLines(linesOfInput); // clear out empty lines
+        
+
+        for (int i = 0; i < linesOfInput.length; i++) {
+            String line = linesOfInput[i];
+
+            // handling an assignment operation
+            if(line.contains("=")) {
+                String[] part = line.split("=");
+                String key = part[0].trim();
+                int value = Integer.parseInt(part[1].trim());
+                Main.values.put(key, value );
+            
+            }
+        }
+        
     }
 }
